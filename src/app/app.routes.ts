437d0e3cdb46765bdbce24533c289b4b44ceb47a
Raly04 <ralyandrynyainakadmiel@gmail.com>
2024-07-11 08:45:16 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path : "home" , loadComponent : () => import("../components/home/home.component"),title:"Home"},
+  {path : "home" , loadComponent : () => import("../components/about-us/about-us.component"),title:"About"}
+];
