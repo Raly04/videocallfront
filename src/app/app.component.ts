8d90581d5466b9e013301bbc45feb86ae5346f63
Raly04 <ralyandrynyainakadmiel@gmail.com>
2024-07-13 13:20:10 +0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,16 @@
-import {Component, inject} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {MatButton, MatIconButton} from "@angular/material/button";
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {MatButton, MatFabButton, MatIconButton} from "@angular/material/button";
 import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
 import {MatIcon} from "@angular/material/icon";
 import {AsyncPipe} from "@angular/common";
-import {ThemeService} from "../services/theme.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconButton, MatMenuTrigger, MatIcon, AsyncPipe, MatMenu, MatMenuItem, MatButton],
+  imports: [RouterOutlet, MatIconButton, MatMenuTrigger, MatIcon, AsyncPipe, MatMenu, MatMenuItem, MatButton, MatFabButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'material';
-  themeManager = inject(ThemeService);
-  isDark$ = this.themeManager.isDark$;
-
-  changeTheme(theme: string) {
-    this.themeManager.changeTheme(theme);
-  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
 export const routes: Routes = [
-  {path : "home" , loadComponent : () => import("../components/home/home.component"),title:"Home"},
-  {path : "home" , loadComponent : () => import("../components/about-us/about-us.component"),title:"About"}
+  {path: "", redirectTo: "home", pathMatch: "full"},
+  {path: "home", loadComponent: () => import("../components/home/home.component"), title: "Home"},
+  {path: "about-us", loadComponent: () => import("../components/about-us/about-us.component"), title: "About"}
 ];
