6242ea661d735d4caaf054d3c0ae8112b268450d
Raly04 <ralyandrynyainakadmiel@gmail.com>
2024-07-15 23:37:38 +0300

    mandamina kely

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path : "" , redirectTo : "home" , pathMatch : "full"},
+  {path : "home" , loadComponent : ()=> import("../components/home/home.component") , title : "Home"},
+  {path : "about-us" , loadComponent : ()=> import("../components/about-us/about-us.component") , title : "About us"},
+];
